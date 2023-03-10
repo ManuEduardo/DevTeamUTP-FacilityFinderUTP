bbb037f9be55ef1d10b296674c4aa96f329d807d
Manuel Sanchez Suyon <manueduardo000@hotmail.com>
2023-03-10 00:50:41 -0500

    funcion de lectura csv

--- backend_FacilityFinderUTP/src/main/java/org/source/Main.java
+++ backend_FacilityFinderUTP/src/main/java/org/source/Main.java
@@ -1,7 +1,23 @@
 package org.source;
 
+import org.source.utils.LecturaCsv;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //TEST PASA LOS CSV
+        LecturaCsv leerCsvAlumnos = new LecturaCsv("src/main/java/org/source/csvs/alumnos.csv");
+        LinkedList<String[]> tablaAlumnos = leerCsvAlumnos.leer();
+        tablaAlumnos.forEach((campos) -> {
+            System.out.println(Arrays.toString(campos));
+        });
+
+        LecturaCsv leerCsvProfesores = new LecturaCsv("src/main/java/org/source/csvs/profesores.csv");
+        LinkedList<String[]> tablaProfesores = leerCsvProfesores.leer();
+        tablaProfesores.forEach((campos) -> {
+            System.out.println(Arrays.toString(campos));
+        });
     }
 }
