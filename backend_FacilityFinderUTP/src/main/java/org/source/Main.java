4b4874e687492bce6ff4f879f4dc93fb36016f95
Manuel Sanchez Suyon <manueduardo000@hotmail.com>
2023-03-21 23:48:44 -0500

    Args main

--- backend_FacilityFinderUTP/src/main/java/org/source/Main.java
+++ backend_FacilityFinderUTP/src/main/java/org/source/Main.java
@@ -11,7 +11,9 @@ public class Main {
     private static final int PORT = 8000;
 
     public static void main(String[] args) throws IOException {
-
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
         // Crear un objeto servidor HTTP en el puerto 8000
         HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
         // Adjuntar un controlador para la ruta "/test"
