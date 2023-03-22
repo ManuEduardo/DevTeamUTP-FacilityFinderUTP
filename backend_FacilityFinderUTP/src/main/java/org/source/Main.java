f6599a1dfac9cd998d4167b5ee99014f9d065e1b
Gabriel Paiva <u21209578@utp.edu.pe>
2023-03-22 11:21:57 -0500

    Edit

---
+++ backend_FacilityFinderUTP/src/main/java/org/source/Main.java
@@ -0,0 +1,96 @@
+package org.source;
+
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.source.servicios.ServicioSalonEstudiante;
+import org.source.servicios.ServicioSalonProfesor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    private static int PORT;
+    private static Options options;
+    private static CommandLine cmd;
+    private static String directorioRaiz;
+
+    private static String separador = System.getProperty("file.separator");
+
+    private static String directorioRecursos = null;
+
+    public static String getDirectorioRaiz() {
+        return directorioRaiz;
+    }
+
+    public static String getSeparador() {
+        return separador;
+    }
+
+    public static String getDirectorioRecursos() {
+        return directorioRecursos;
+    }
+
+    private static void crearOpciones() throws Exception {
+        //Generando Opciones de CMD
+        options = new Options();
+        options.addOption("h", "help", false, "Muestra ayuda");
+        options.addOption("r", "resources", true, "Ingresa el Directorio de Recursos");
+        options.addOption("p", "port", true, "Especifica el puerto para ejecutar.");
+    }
+
+    private static String obtenerDirectorioRecursos(String[] args) throws Exception {
+
+        try {
+            cmd = new DefaultParser().parse(options, args);
+            if (cmd.hasOption("r")) {
+                directorioRecursos = cmd.getOptionValue("r");
+            }
+        } catch (Exception e) {
+            String msg = "Ruta de Directorio de Recursos No ingresada";
+            throw new Exception(msg);
+        }finally {
+            return directorioRecursos;
+        }
+
+    }
+
+    private static int obtenerPuerto(String[] args)throws Exception{
+        int puerto = 8000;
+
+        try {
+            cmd = new DefaultParser().parse(options, args);
+            if (cmd.hasOption("p")) {
+                puerto = Integer.parseInt(cmd.getOptionValue("p"));
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            return puerto;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+            crearOpciones();
+            directorioRaiz = obtenerDirectorioRecursos(args);
+            PORT = obtenerPuerto(args);
+            // Crear un objeto servidor HTTP en el puerto 8000
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            // Adjuntar un controlador para la ruta "/test"
+            server.createContext("/estudiante", new ServicioSalonEstudiante());
+            server.createContext("/profesor", new ServicioSalonProfesor());
+            // Iniciar el servidor
+            server.start();
+            System.out.println("Servidor iniciado en el puerto 8000...");
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
---
+++ backend_FacilityFinderUTP/src/main/java/org/source/utils/Constantes.java
@@ -0,0 +1,93 @@
+package org.source.utils;
+import org.source.Main;
+import org.source.modelos.Estudiante;
+import org.source.modelos.Profesor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Esta es la descripción de la clase Constantes:
+ * @author Gabriel Paiva
+ * @version 1.0
+ * @since 2023-03-19
+ */
+
+public class Constantes {
+
+    /**
+     * Rutas de archivo que representan la ubicación de dos archivos en el proyecto.:
+     */
+    private static final String nombreCarpetaData = "csvs";
+    private static final String nombreArchivoDataHorarios = "csvs/DataHorarios.csv";
+    private static final String nombreCarpetaErrorLog = "";
+    private static final String nombreArchivoErrorLog = "error.log";
+    private static final Path ErrorLog = Paths.get("src", "main", "resources", "error.log");
+    public static String RutaCsvDataHorarios() {
+        return String.format("%s%s%s%s%s",Main.getDirectorioRecursos(),Main.getSeparador(),nombreCarpetaData,Main.getSeparador(),nombreArchivoDataHorarios);
+    }
+    public static String RutaErrorLog() {
+        return String.format("%s%s%s%s%s",Main.getDirectorioRecursos(),Main.getSeparador(),nombreArchivoErrorLog,Main.getSeparador(),nombreArchivoErrorLog);
+    }
+
+    /**
+     * Creación de un objeto de la clase LecturaCsv utilizando el método RutaCsvDataHorarios()
+     * para obtener la ruta del archivo CSV que se utilizará para la lectura.
+     */
+    private static LecturaCsv lecturaCsv = new LecturaCsv(RutaCsvDataHorarios());
+
+    /**
+     * Obtenermos la data, como es algo que está en una "base de datos" csv, entonces
+     * será estatica. En el primer Hashmap estará la data de los alumnos, en el
+     * segundo la data de los profesores. Finalmente, convertiremos de clase Object a la
+     * clase que corresponde, o profesor o estudiante.
+     */
+    private static ProcesarCsv totalData = new ProcesarCsv(lecturaCsv.leer()); // Constructor de la clase ProcesarCsv
+    public static HashMap <String,Object>[] dataProcesada() throws IOException {
+        return totalData.createData();
+    }
+    public static HashMap <String,Profesor> dataProfesor() throws IOException {
+        return convertirHashMapProfesor(dataProcesada()[1]);
+    }
+    public static HashMap <String,Estudiante> dataEstudiante() throws IOException {
+        return convertirHashMapEstudiante(dataProcesada()[0]);
+    }
+    public static HashMap<String, Estudiante> convertirHashMapEstudiante(HashMap<String, Object> mapa) {
+
+        // Primero, se crea un nuevo HashMap que tendrá las mismas claves que el
+        // HashMap original, pero con valores de tipo Estudiante.
+        HashMap<String, Estudiante> nuevoMapa = new HashMap<>();
+
+        // Luego, se recorre cada entrada del HashMap original utilizando
+        // un ciclo for-each.
+        for (Map.Entry<String, Object> entry : mapa.entrySet()) {
+
+            // Dentro del ciclo, se obtiene la clave y el valor correspondientes
+            // a cada entrada.
+            String clave = entry.getKey();
+            Estudiante estudiante = (Estudiante) entry.getValue(); // casting explícito a la clase Estudiante
+            nuevoMapa.put(clave, estudiante);
+
+        }
+
+        return nuevoMapa;
+
+    }
+    public static HashMap<String, Profesor> convertirHashMapProfesor(HashMap<String, Object> mapa) {
+
+        // Funciona de la misma manera que: convertirHashMapEstudiante pero con el objeto Profesor.
+        HashMap<String, Profesor> nuevoMapa = new HashMap<>();
+
+        for (Map.Entry<String, Object> entry : mapa.entrySet()) {
+            String clave = entry.getKey();
+            Profesor profesor= (Profesor) entry.getValue(); // casting explícito a la clase Estudiante
+            nuevoMapa.put(clave, profesor);
+        }
+
+        return nuevoMapa;
+
+    }
+}
